f80dee80f1284c2298717506d93b94c655b6bb88
Samuel Georges <sam@daftspunk.com>
2015-06-20 23:25:45 +1000

    Add banner support for pages Style up footer menu Make contact form functional Populate some sample chair content

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,56 @@
 /*
  * Application
  */
+
+/*
+ * Implement nice invalid form fields
+ */
+$(window).on('ajaxInvalidField', function(event, fieldElement, fieldName, errorMsg, isFirst) {
+    event.preventDefault()
+
+    var $field = $(fieldElement).closest('.form-group'),
+        $help = $('<p />').addClass('help-block')
+
+    if (!$field.length) {
+        $field = fieldElement.closest('.field')
+    }
+
+    if (!$field.length) {
+        return
+    }
+
+    if (errorMsg) {
+        $help.text(errorMsg.join(', '))
+    }
+
+    $help.addClass('form-field-error-label')
+    $field.addClass('has-error')
+
+    // Prevent the next error alert only once
+    $(window).one('ajaxErrorMessage', function(event, message){
+        event.preventDefault()
+    })
+
+    if ($('.form-field-error-label', $field).length > 0)
+        return
+
+    $field.append($help)
+
+    // Scroll to the form group
+    if (isFirst) {
+        $('html, body').animate({ scrollTop: $field.offset().top }, 500, function(){
+            fieldElement.focus()
+        })
+    }
+})
+
+$(document).on('ajaxPromise', '[data-request]', function() {
+    var $form = $(this).closest('form'),
+        $help = $('.form-field-error-label', $form)
+
+    if (!$form.length || !$help.length)
+        return
+
+    $help.remove()
+    $('.form-group.has-error', $form).removeClass('has-error')
+})
